50831938eaa188599af17b8bcbcaa62f73c017cd
rahul <rdev05.rd@gmail.com>
2025-03-05 12:41:42 +0530

    added Tailwindcss to our project

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Body = () => {
-  return (
-    <div>Body</div>
-  )
-}
+  return <div className="font-bold">Body</div>;
+};
 
-export default Body
+export default Body;
